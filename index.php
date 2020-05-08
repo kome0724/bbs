f6c1376ad4d32555eb1be4ef86a24166a55bdc3e
kome0724 <kome_engineer@ymobile.ne.jp>
2020-05-08 22:59:39 +0900

    twitter風

--- index.php
+++ index.php
@@ -1,5 +1,45 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
+	$_SESSION['time'] = time();
+
+	$members = $db->prepare('SELECT * FROM members WHERE id=?');
+	$members->execute(array($_SESSION['id']));
+	$member = $members->fetch();
+} else {
+	header('Location: login.php');
+	exit();
+}
+
+if (!empty($_POST)) {
+	if ($_POST['message'] !== '') {
+		$message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
+		$message->execute(array(
+			$member['id'],
+			$_POST['message']
+		));
+
+		header('Location: index.php'); //POSTの中身をなくす
+		exit();
+	}
+}
+
+$posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+
+if(isset($_REQUEST['res'])){
+	//返信処理
+	$response = $db->prepare('SELECT m.name, m.piucture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=?');
+	$response->execute(array($_REQUEST['res']));
+
+	$table = $response->fetch();
+	$message = '@'. $table['name']. ' '. $table['message'];
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,43 +50,45 @@
 </head>
 
 <body>
-<div id="wrap">
-  <div id="head">
-    <h1>ひとこと掲示板</h1>
-  </div>
-  <div id="content">
-  	<div style="text-align: right"><a href="logout.php">ログアウト</a></div>
-    <form action="" method="post">
-      <dl>
-        <dt>○○さん、メッセージをどうぞ</dt>
-        <dd>
-          <textarea name="message" cols="50" rows="5"></textarea>
-          <input type="hidden" name="reply_post_id" value="" />
-        </dd>
-      </dl>
-      <div>
-        <p>
-          <input type="submit" value="投稿する" />
-        </p>
-      </div>
-    </form>
+	<div id="wrap">
+		<div id="head">
+			<h1>ひとこと掲示板</h1>
+		</div>
+		<div id="content">
+			<div style="text-align: right"><a href="logout.php">ログアウト</a></div>
+			<form action="" method="post">
+				<dl>
+					<dt><?php print(htmlspecialchars($member['name'], ENT_QUOTES)); ?>さん、メッセージをどうぞ</dt>
+					<dd>
+						<textarea name="message" cols="50" rows="5"><?php print(htmlspecialchars($message, ENT_QUOTES));?></textarea>
+						<input type="hidden" name="reply_post_id" value="" />
+					</dd>
+				</dl>
+				<div>
+					<p>
+						<input type="submit" value="投稿する" />
+					</p>
+				</div>
+			</form>
 
-    <div class="msg">
-    <img src="member_picture" width="48" height="48" alt="" />
-    <p><span class="name">（）</span>[<a href="index.php?res=">Re</a>]</p>
-    <p class="day"><a href="view.php?id="></a>
-<a href="view.php?id=">
-返信元のメッセージ</a>
-[<a href="delete.php?id="
-style="color: #F33;">削除</a>]
-    </p>
-    </div>
+			<?php foreach ($posts as $post): ?>
+				<div class="msg">
+					<img src="member_picture/<?php print(htmlspecialchars($post['picture'], ENT_QUOTES)); ?>" width="48" height="48" alt="" />
+					<p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)); ?>">Re</a>]</p>
+					<p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
+						<a href="view.php?id=">
+							返信元のメッセージ</a>
+						[<a href="delete.php?id=" style="color: #F33;">削除</a>]
+					</p>
+				</div>
+			<?php endforeach; ?>
 
-<ul class="paging">
-<li><a href="index.php?page=">前のページへ</a></li>
-<li><a href="index.php?page=">次のページへ</a></li>
-</ul>
-  </div>
-</div>
+			<ul class="paging">
+				<li><a href="index.php?page=">前のページへ</a></li>
+				<li><a href="index.php?page=">次のページへ</a></li>
+			</ul>
+		</div>
+	</div>
 </body>
+
 </html>
